addef552cd0f305a050956fe7b3b546902fee6d0
hari <harirs@gmail.com>
2023-01-10 08:24:32 +0530

    first commit

---
+++ content/collections/pages/home.md
@@ -0,0 +1,9 @@
+---
+id: home
+blueprint: pages
+title: Home
+template: home
+author: 0e9ced9a-2063-4cb9-9e8a-9bca000c08f7
+updated_by: 0e9ced9a-2063-4cb9-9e8a-9bca000c08f7
+updated_at: 1673280218
+---
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    './resources/**/*.antlers.html',
+    './resources/**/*.blade.php',
+    './content/**/*.md'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#12151E',
+        // 'hot-pink': '#fd2d78'
+        'hot-pink': '#fd2d78'
+      },
+      fontFamily: {
+        display: "var(--font-display)",
+        body: "var(--font-body)",
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+  important: true
+}
